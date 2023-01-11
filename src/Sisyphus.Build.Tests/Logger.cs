7fc1501465115613d34d52ac41ab59de19f7e9a5
Tomat <xxlennygamerxx@gmail.com>
2023-01-10 22:28:11 -0800

    Begin work on path resolution.

---
+++ src/Sisyphus.Build.Tests/Logger.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sisyphus.Build.Tests;
+
+internal static class Logger {
+    private sealed class LoggerBuildEngine : IBuildEngine {
+        private string Name { get; }
+
+        public LoggerBuildEngine(string name) {
+            Name = name;
+        }
+
+        // TODO: Better formatting.
+        private string DirtyFormat(
+            DateTime time,
+            string cat,
+            string? msg,
+            string level
+        ) {
+            return $"[{time}] [{level}] {Name} ({cat}): {msg}";
+        }
+
+        void IBuildEngine.LogErrorEvent(BuildErrorEventArgs e) {
+            /*
+             *             var time = e.Timestamp;
+            var name = e.File;
+            var cat = e.Subcategory;
+            var msg = e.Message ?? "<no message>";
+             */
+            Console.WriteLine(
+                DirtyFormat(
+                    e.Timestamp,
+                    e.Subcategory,
+                    e.Message,
+                    "Error"
+                )
+            );
+        }
+
+        void IBuildEngine.LogWarningEvent(BuildWarningEventArgs e) {
+            Console.WriteLine(
+                DirtyFormat(
+                    e.Timestamp,
+                    e.Subcategory,
+                    e.Message,
+                    "Warning"
+                )
+            );
+        }
+
+        void IBuildEngine.LogMessageEvent(BuildMessageEventArgs e) {
+            Console.WriteLine(
+                DirtyFormat(
+                    e.Timestamp,
+                    e.Subcategory,
+                    e.Message,
+                    "Message"
+                )
+            );
+        }
+
+        void IBuildEngine.LogCustomEvent(CustomBuildEventArgs e) {
+            Console.WriteLine(
+                DirtyFormat(
+                    e.Timestamp,
+                    "n/a",
+                    e.Message,
+                    "Custom"
+                )
+            );
+        }
+
+        bool IBuildEngine.BuildProjectFile(
+            string projectFileName,
+            string[] targetNames,
+            IDictionary globalProperties,
+            IDictionary targetOutputs
+        ) {
+            return false;
+        }
+
+        bool IBuildEngine.ContinueOnError => true;
+
+        int IBuildEngine.LineNumberOfTaskNode => 0;
+
+        int IBuildEngine.ColumnNumberOfTaskNode => 0;
+
+        string IBuildEngine.ProjectFileOfTaskNode => "<unknown>";
+    }
+
+    private sealed class LoggerTaskHost : ITaskHost { }
+
+    private sealed class LoggerTask : ITask {
+        public LoggerTask(string name) {
+            BuildEngine = new LoggerBuildEngine(name);
+        }
+
+        bool ITask.Execute() {
+            return true;
+        }
+
+        public IBuildEngine BuildEngine { get; set; }
+
+        ITaskHost ITask.HostObject { get; set; } = new LoggerTaskHost();
+    }
+
+    public static TaskLoggingHelper Create(string name) {
+        return new TaskLoggingHelper(new LoggerTask(name));
+    }
+}
---
+++ src/Sisyphus.Build/Util/PathResolution.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Sisyphus.Build.Util;
+
+/// <summary>
+///     Utilities for resolving paths relating to ULTRAKILL and sispyhus.
+/// </summary>
+public static class PathResolution {
+    public const string SISYPHUS_UK_GAME_PATH = "SISYPHUS_UK_GAME_PATH";
+    public const string SISYPHUS_UK_ASSEMBLY_PATH = "SISYPHUS_UK_ASSEMBLY_PATH";
+
+    /// <summary>
+    ///     Retrieves the path to the directory containing an installation of
+    ///     ULTRAKILL.
+    /// </summary>
+    /// <param name="log">The logger to use.</param>
+    /// <returns>
+    ///     The path, or <see langword="null"/> if no path is resolved and no
+    ///     fallback is provided.
+    /// </returns>
+    public static string? GetGamePath(TaskLoggingHelper log) {
+        var path = GetGamePath_Inner(log);
+        
+        if (path is null) {
+            log.LogError("Failed to resolve ULTRAKILL game path.");
+            return null;
+        }
+        
+        log.LogMessage($"Resolved ULTRAKILL game path: {path}");
+        return path;
+    }
+
+    private static string? GetGamePath_Inner(TaskLoggingHelper log) {
+        log.LogMessage("Resolving ULTRAKILL game path...");
+        log.LogMessage("Checking env. var.: " + SISYPHUS_UK_GAME_PATH);
+        var path = Environment.GetEnvironmentVariable(SISYPHUS_UK_GAME_PATH);
+        
+        if (path != null) {
+            log.LogMessage("Resolved path from env. var.: " + path);
+            return path;
+        }
+        
+        log.LogMessage("No path found in env. var., resolving automatically.");
+        return null; // TODO: Implement automatic resolution for game path.
+    }
+
+    /// <summary>
+    ///     Retrieves the path to the directory containing .NET assemblies of
+    ///     ULTRAKILL and adjacent libraries, often within
+    ///     <see cref="GetGamePath"/> but optionally not always.
+    /// </summary>
+    /// <param name="log">The logger to use.</param>
+    /// <returns>
+    ///     The path, or <see langword="null"/> if no path is resolved and no
+    ///     fallback is provided.
+    /// </returns>
+    public static string? GetAssemblyPath(TaskLoggingHelper log) {
+        var path = GetAssemblyPath_Inner(log);
+        
+        if (path is null) {
+            log.LogError("Failed to resolve ULTRAKILL assembly path.");
+            return null;
+        }
+        
+        log.LogMessage($"Resolved ULTRAKILL assembly path: {path}");
+        return path;
+    }
+    
+    private static string? GetAssemblyPath_Inner(TaskLoggingHelper log) {
+        log.LogMessage("Resolving ULTRAKILL assemblies path...");
+        log.LogMessage("Checking env. var.: " + SISYPHUS_UK_ASSEMBLY_PATH);
+        var path = Environment.GetEnvironmentVariable(SISYPHUS_UK_ASSEMBLY_PATH);
+        
+        if (path != null) {
+            log.LogMessage("Resolved path from env. var.: " + path);
+            return path;
+        }
+        
+        log.LogMessage("No path found in env. var., resolving automatically.");
+        return null; // TODO: Implement automatic resolution for assembly path.
+    }
+}
